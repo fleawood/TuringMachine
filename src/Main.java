8003e92e40589de92b5f6a9eb1218d7317b5787b
fleawood <fleawood@users.noreply.github.com>
2018-12-19 11:45:19 +0800

    create TuringMachine project

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(@NotNull String[] args) {
+        if (args.length != 2) {
+            System.out.println("Error");
+            return;
+        }
+        String tmFileName = args[0];
+        String inputFileName = args[1];
+
+        String[] tmLines = readFile(tmFileName);
+
+    }
+
+    private static String[] readFile(String FileName) {
+        Path file = Paths.get(FileName);
+        Charset charset = StandardCharsets.US_ASCII;
+        ArrayList<String> list = new ArrayList<>();
+        try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                list.add(line);
+            }
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
+        return list.toArray(new String[0]);
+    }
+}
---
+++ src/Parser.java
@@ -0,0 +1,8 @@
+public class Parser {
+    public void parse(String program) {
+
+    }
+    public static void main(String[] args) {
+        System.out.println("You are debugging `Parser`.");
+    }
+}
